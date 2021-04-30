0cbff1017a16bdbd1476ee88b94e4f3cef90325a
Richard Durazo <rcdurazo@gmail.com>
2021-04-30 11:32:29 -0700

    signup form updates

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import CommonButton from './ui/CommonButton';
+import AuthForm from './AuthForm';
 
-const Auth = () => {
+const Auth = props => {
     return (
         <section className='auth flex-column'>
             <h3>Sign Up</h3>
-            <input type='email' />
-            <input type='password' />
-            <CommonButton buttonText={'Sign Up'} />
+            <AuthForm />
         </section>
     );
 };
